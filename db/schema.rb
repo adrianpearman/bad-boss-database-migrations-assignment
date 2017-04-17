dfb5381aae670ad2408b75e01d884e3c233da8a9
Adrian Pearman <adrianpearman12@gmail.com>
2017-04-16 23:33:39 -0400

    create location table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170417023342) do
+ActiveRecord::Schema.define(version: 20170417032806) do
+
+  create_table "locations", force: :cascade do |t|
+    t.string  "city"
+    t.string  "state_or_province"
+    t.string  "country"
+    t.integer "population"
+    t.integer "size"
+  end
 
   create_table "parts", force: :cascade do |t|
     t.string  "name"
